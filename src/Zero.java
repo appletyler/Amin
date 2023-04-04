6e2d495225f95f423ff46f271065eca5c570f2a2
appletyler <apple_tyler@mail.ru>
2023-04-04 21:13:48 +0300

    Initial commit

---
+++ src/Zero.java
@@ -0,0 +1,13 @@
+public class Zero {
+    public int stack(int num1, int num2){
+        int a = num1+num2;
+        return a;
+    }
+    public int multiply (int num1, int num2){
+        int b = num1*num2;
+        return b;
+    }
+    public int result (int a, int b){
+        return (int)(Math.max(a, b));
+    }
+}
